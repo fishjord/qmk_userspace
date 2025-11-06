8cf515fcf257901000a11708d598572d5f2c50ba
Jordan Fish <jrdn.fish@gmail.com>
2025-11-06 08:11:25 -0800

    Initial personal keymap for the sofle pico

---
+++ keyboards/sofle_pico/keymap/nes/config.h
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define POINTING_DEVICE_ROTATION_90
+#define SPLIT_POINTING_ENABLE
+#define POINTING_DEVICE_RIGHT
+#define POINTING_DEVICE_TASK_THROTTLE_MS 8
